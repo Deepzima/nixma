60552ed3722c553a2e97802dfecffb984bf9a509
Deepzima <deepzima@outlook.com>
2024-07-08 14:43:52 +0200

    feat: add more than one test flake

---
+++ examples/golang/simple-hello/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A simple Go program";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "aarch64-darwin" "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = system: import nixpkgs { inherit system; };
+      
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = pkgsFor system;
+          # src = pkgs.lib.cleanSourceWith {
+          #   filter = path: type: type == "source" || builtins.elem (baseNameOf path) [ "flake.nix" "go.mod" "main.go" ];
+          #   src = ./.;
+          # };
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            name = "hello";
+            src = ./.;
+            buildInputs = [ pkgs.go ];
+            buildPhase = ''
+              export GOCACHE=$(mktemp -d)
+              ls -la
+              go build -o hello main.go
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              install -t $out/bin hello
+            '';
+          };
+        });
+    };
+}
